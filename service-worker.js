6f75eb8db0e7ecb771eeb1257f7bb246e09d0877
gitlab-runner <kevin.colin@jojoex.com>
2023-08-25 03:51:16 +0000

    release

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
 importScripts(
-  "/precache-manifest.ebcf6c9bab665768fdf50110971986e9.js"
+  "/precache-manifest.fe15b3e52ffa064e8981e47b2448c57a.js"
 );
 
 workbox.skipWaiting();
